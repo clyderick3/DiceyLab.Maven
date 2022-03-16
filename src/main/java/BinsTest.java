772d0c84821c7e39565e8230cf85feccf057bfbc
Clyde Broderick <clyderick3@gmail.com>
2022-03-15 23:16:17 -0400

    Created simulation.

---
+++ src/main/java/BinsTest.java
@@ -0,0 +1,27 @@
+import org.junit.Test;
+
+public class BinsTest {
+
+@Test
+public void incrementBinTest() {
+//    //Given
+//    int
+//    //When
+//
+//    //Then
+//    int actual = ;
+//    int expected = ;
+
+
+    }
+
+@Test
+public void getBin() {
+    //Given
+
+    //When
+
+    //Then
+
+    }
+}
--- src/main/java/Simulation.java
+++ src/main/java/Simulation.java
@@ -3,14 +3,17 @@ public class Simulation {
     //psvm
     public static void main(String[] args) {
 
-        Dice dice = new Dice(2);
-        for (int i = 0; i < 10 ; i++) {
-            //System.out.println(dice.tossAndSum());
-            Integer result = dice.tossAndSum();
-            if (result >= 10) {
-                System.out.println(result);
+        Simulation sim = new Simulation(2, 1000000);
+        sim.runSimulations();
+        sim.printResults();
+//        Dice dice = new Dice(2);
+//        for (int i = 0; i < 10 ; i++) {
+//            //System.out.println(dice.tossAndSum());
+//            Integer result = dice.tossAndSum();
+//            if (result >= 10) {
+//                System.out.println(result);
+
+                //Create a simulation where two die are throw 1,000,000 times.
             }
         }
-    }
 
-}
