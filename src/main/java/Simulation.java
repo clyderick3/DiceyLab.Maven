a2be4501e95c25ddf0c6fbd08c96284738922902
Clyde Broderick <clyderick3@gmail.com>
2022-03-25 10:45:30 -0400

    Added void run method.

--- src/main/java/Simulation.java
+++ src/main/java/Simulation.java
@@ -1,19 +1,33 @@
 public class Simulation {
-
-    //psvm
     public static void main(String[] args) {
+        run(2,1000000);
+    }
 
-//        Simulation sim = new Simulation(2, 1000000);
-//        sim.runSimulations();
-//        sim.printResults();
-//        Dice dice = new Dice(2);
-//        for (int i = 0; i < 10 ; i++) {
-//            //System.out.println(dice.tossAndSum());
-//            Integer result = dice.tossAndSum();
-//            if (result >= 10) {
-//                System.out.println(result);
+
+    //psvm = public static void method
+    public static void run (Integer numOfDice, Integer numOfToss) {
+        Dice dice = new Dice (numOfDice);
+        Bins bins = new Bins (); //
+
+        for (int i = 0; i < numOfToss; i++){
+            dice.tossAndSum();
+            bins.incrementBin(dice.tossAndSum());
+        }
+
+    }
+        //doesn't see a constructor that accepts two arguments.
+        Simulation sim = new Simulation(2, 1000000);
+        sim.runSimulations();
+        sim.printResults();
+        Dice dice = new Dice(2);
+        for (int i = 0; i < 10; i++) {
+            //System.out.println(dice.tossAndSum());
+            Integer result = dice.tossAndSum();
+            if (result >= 10) {
+                System.out.println(result);
 
                 //Create a simulation where two die are throw 1,000,000 times.
             }
         }
+    }
 
